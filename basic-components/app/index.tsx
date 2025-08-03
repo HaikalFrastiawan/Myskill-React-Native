c2aab65b94356edba53ccc98f4b2d82a83f67ab9
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-08-03 14:34:33 +0700

    perubahan

--- basic-components/app/index.tsx
+++ basic-components/app/index.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from "react-native";
+
+import { SafeAreaView, Text, View,Button, TouchableOpacity } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-
-      <Text>Home screen.</Text>
-    </View>
+   <View>
+    <Text>
+      
+    </Text>
+    <TouchableOpacity>
+      <Text>Click me</Text>
+    </TouchableOpacity>
+      <Button title="login" onPress={()=> alert("ini button")}/>
+   </View>
   );
 }
